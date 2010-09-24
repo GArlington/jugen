ab4734c1af46dc75f0e09ea1978dedc543b33580
fredy.wijaya <fredy.wijaya@gmail.com>
2010-09-24 15:46:40 +0000

    - Add overwrite option - Add Apache 2.0 license

--- src/main/java/org/fredy/jugen/core/JUGenCommand.java
+++ src/main/java/org/fredy/jugen/core/JUGenCommand.java
@@ -1,9 +1,27 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.fredy.jugen.core;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.logging.Logger;
 
 import org.fredy.jugen.core.util.JUGenUtils;
 
@@ -28,12 +46,22 @@ public class JUGenCommand implements Command {
         if (!dir.exists()) {
             dir.mkdirs();
         }
+        File junitFile = new File(dir, result.getJavaObjectInfo().getClassName() + ".java");
+        if (junitFile.exists()) {
+            if (param.isOverwrite()) {
+                createFile(junitFile, result.getJUnitContent());
+            }
+        } else {
+            createFile(junitFile, result.getJUnitContent());
+        }
+    }
+    
+    private void createFile(File file, String content) {
         PrintWriter pw = null;
         try {
-            File junitFile = new File(dir, result.getJavaObjectInfo().getClassName() + ".java");
-            System.out.println("Creating " + junitFile);
-            pw = new PrintWriter(junitFile);
-            pw.print(result.getJUnitContent());
+            System.out.println("Creating " + file);
+            pw = new PrintWriter(file);
+            pw.print(content);
         } catch (FileNotFoundException e) {
             throw new JUGenException(e);
         } finally {
