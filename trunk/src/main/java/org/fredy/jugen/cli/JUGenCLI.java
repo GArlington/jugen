f24706c0d1f55ea89ffd8613586723393b2ba7b2
fredy.wijaya@gmail.com <fredy.wijaya@gmail.com@9ba2f40c-2b30-664e-4333-638784bcd896>
2010-09-24 15:46:40 +0000

    - Add overwrite option - Add Apache 2.0 license

--- trunk/src/main/java/org/fredy/jugen/cli/JUGenCLI.java
+++ trunk/src/main/java/org/fredy/jugen/cli/JUGenCLI.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.fredy.jugen.cli;
 
 import java.io.File;
@@ -16,10 +35,12 @@ public class JUGenCLI implements Executable {
 
     private static final String JUNIT4_VERSION = "4";
     private static final String JUNIT3_VERSION = "3";
+    private static final String OVERWRITE = "overwrite";
     private String projectDir;
     private String destinationDir;
     private String templateDir;
     private Template template;
+    private boolean overwrite;
     private JUGen jugen = new JUGen();
     
     public JUGenCLI(String[] args) {
@@ -27,6 +48,7 @@ public class JUGenCLI implements Executable {
         projectDir = args[0];
         destinationDir = args[1];
         templateDir = args[2];
+        overwrite = (args.length == 5) ? true : false;
         if (JUNIT3_VERSION.equals(args[3].trim())) {
             template = Template.JUNIT3;
         } else {
@@ -41,26 +63,32 @@ public class JUGenCLI implements Executable {
         param.setDestDir(new File(destinationDir));
         param.setTemplateDir(new File(templateDir));
         param.setTemplate(template);
+        param.setOverwrite(overwrite);
         jugen.generateJUnit(param);
     }
     
     private void validateArgs(String[] args) {
-        if (args.length != 4) {
+        if (args.length < 4) {
             throw new JUGenException(getUsage());
         }
         if (!new File(args[0]).isDirectory()) {
-            throw new JUGenException(getUsage());
+            throw new JUGenException(args[0] + " is not a directory");
         }
         if (!new File(args[2]).isDirectory()) {
-            throw new JUGenException(getUsage());
+            throw new JUGenException(args[2] + " is not a directory");
         } 
         if (!JUNIT3_VERSION.equals(args[3].trim()) && !JUNIT4_VERSION.equals(args[3].trim())) {
-            throw new JUGenException(getUsage());
+            throw new JUGenException(args[3] + " must be either 3 or 4");
         } 
+        if (args.length == 5) {
+            if (!OVERWRITE.equals(args[4])) {
+                throw new JUGenException("Only overwrite keyword is allowed");
+            }
+        }
     }
 
     private String getUsage() {
-        return "Usage: java -jar JUGenCLI <project_dir> <destination_dir> " +
-        		"<template_dir> <junit_version>";
+        return "Usage: java -jar jugen.jar <project_dir> <destination_dir> " +
+        		"<template_dir> <junit_version> [overwrite]";
     }
 }
--- trunk/src/test/java/org/fredy/jugen/core/JUGenTest.java
+++ trunk/src/test/java/org/fredy/jugen/core/JUGenTest.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.fredy.jugen.core;
 
 import java.io.File;
@@ -22,6 +41,7 @@ public class JUGenTest {
         param.setDestDir(new File("result/j4a"));
         param.setTemplateDir(new File("templates"));
         param.setTemplate(Template.JUNIT4);
+        param.setOverwrite(false);
         jugen.generateJUnit(param);
         assertTrue(new File("result/j4a/org/fredy/jugen/core/JavaObjectInfoTest.java").exists());
         assertTrue(new File("result/j4a/org/fredy/jugen/core/JUGenTest.java").exists());
@@ -37,6 +57,7 @@ public class JUGenTest {
         param.setDestDir(new File("result/j3a"));
         param.setTemplateDir(new File("templates"));
         param.setTemplate(Template.JUNIT3);
+        param.setOverwrite(false);
         jugen.generateJUnit(param);
         assertTrue(new File("result/j3a/org/fredy/jugen/core/JavaObjectInfoTest.java").exists());
         assertTrue(new File("result/j3a/org/fredy/jugen/core/JUGenTest.java").exists());
